b8d0d54d440656e4f6718be8f5cd258ffefc00ee
marhoatumu <52777437+marhoatumu@users.noreply.github.com>
2022-09-20 11:25:41 +0100

    Created endpoints for listing questions and delete

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -89,6 +89,24 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
+    #Endpoint for displaying all available questions page by page
+    @app.route('/questions', methods=['GET'])
+    def get_questions():
+        question_selection = Question.query.order_by(Question.id).all()
+        formatted_questions = paginate_questions(request, question_selection)
+
+        if len(formatted_questions) == 0:
+            abort(404)
+        
+        category_selection = Category.query.order_by(Category.id).all()
+
+        return jsonify({
+            'success':True,
+            'questions':formatted_questions,
+            'total_questions':len(Question.query.all()),
+            'current_category':'No selected Category',
+            'categories':{category.id: category.type for category in category_selection}
+            })
 
     """
     @TODO:
@@ -97,6 +115,31 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+    #Endpoint for deleting a qustion
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
+        try:
+            question = Question.query.filter(Question.id==question_id).one_or_none()
+            if question is None:
+                abort(404)
+            
+            question.delete()
+
+            question_selection = Question.query.order_by(Question.id).all()
+            formatted_questions = paginate_questions(request, question_selection)
+
+            category_selection = Category.query.order_by(Category.id).all()
+
+            return jsonify({
+                'success':True,
+                'deleted': question_id,
+                'questions':formatted_questions,
+                'total_questions':len(Question.query.all()),
+                'categories':{category.id: category.type for category in category_selection}
+                })
+        
+        except:
+            abort(422)
 
     """
     @TODO:
